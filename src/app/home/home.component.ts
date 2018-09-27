0298b9e4c90472a88de8049956c55bcaef194000
opet <opet@Academico.edu>
2018-09-26 21:48:33 -0300

    First Commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount : number;
+  goalText : String = 'My first life goal';
+  goals = [];
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+
+}
